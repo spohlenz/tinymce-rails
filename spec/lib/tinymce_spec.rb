2cf938c5a457675e3d13ad18ea926fefcfbc88e3
Sam Pohlenz <sam@sampohlenz.com>
2012-02-21 11:32:35 +1030

    Add specs for asset hosts and relative url root

--- spec/lib/tinymce_spec.rb
+++ spec/lib/tinymce_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe TinyMCE do
+  before(:each) do
+    Rails.application.config.action_controller.asset_host = nil
+    ActionController::Base.config.relative_url_root = nil
+  end
+  
   it "has a base path" do
     TinyMCE.base.should eq("/assets/tinymce")
+  end
+  
+  it "uses the asset host in the base path when set" do
+    Rails.application.config.action_controller.asset_host = "http://assets.example.com"
+    TinyMCE.base.should eq("http://assets.example.com/assets/tinymce")
+  end
+  
+  it "uses the relative url root in the base path when set" do
+    ActionController::Base.config.relative_url_root = "/subfolder"
+    TinyMCE.base.should eq("/subfolder/assets/tinymce")
   end
 end
