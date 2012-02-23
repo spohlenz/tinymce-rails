bb4fec39ee943aea3fa9d1a44895aebdc2ada1ff
Sam Pohlenz <sam@sampohlenz.com>
2012-02-23 12:40:21 +1030

    Add config.tinymce.base option to override base path

--- lib/tinymce/railtie.rb
+++ lib/tinymce/railtie.rb
@@ -1,11 +1,14 @@
 module TinyMCE
   def self.base
-    [Rails.application.config.action_controller.asset_host,
-     ActionController::Base.config.relative_url_root,
-     Rails.application.config.assets.prefix, "/tinymce"].compact.join
+    Railtie.config.tinymce.base ||
+      [Rails.application.config.action_controller.asset_host,
+       ActionController::Base.config.relative_url_root,
+       Rails.application.config.assets.prefix, "/tinymce"].compact.join
   end
   
   class Railtie < Rails::Railtie
+    config.tinymce = ActiveSupport::OrderedOptions.new
+    
     def asset_root
       File.join(File.dirname(__FILE__), "../../assets")
     end
