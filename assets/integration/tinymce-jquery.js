1492e93951bf328641850a1b76de67f02fd1d781
Ryan Ahearn <ryan@coshx.com>
2012-02-23 10:49:43 -0500

    Moves assets/ files to vendor/
    changes the `rake update` task to expand into
    vendor/assets/javascripts/tinymce/

--- assets/integration/tinymce-jquery.js
+++
@@ -1,3 +0,0 @@
-//= require tinymce/preinit.js
-//= require tinymce/tiny_mce_jquery.js
-//= require tinymce/jquery-tinymce.js
---
+++ vendor/assets/javascripts/tinymce.js
@@ -0,0 +1,2 @@
+//= require preinit.js
+//= require tinymce/tiny_mce_src.js
