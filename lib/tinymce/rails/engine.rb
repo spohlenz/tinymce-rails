8e0a6963a5a835c438b51ec2c956d83704a795b9
Sam Pohlenz <sam@sampohlenz.com>
2012-02-24 20:55:43 +1030

    Move rake task into lib/tasks

--- lib/tinymce/rails/engine.rb
+++ lib/tinymce/rails/engine.rb
@@ -2,10 +2,6 @@ module TinyMCE
   module Rails
     class Engine < ::Rails::Engine
       config.tinymce = ActiveSupport::OrderedOptions.new
-
-      rake_tasks do
-        load File.join(File.dirname(__FILE__), 'assets.rake')
-      end
     end
   end
 end
