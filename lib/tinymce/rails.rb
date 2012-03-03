1d53cb9c88faff4dc3ccc251741fed7b377fad34
Sam Pohlenz <sam@sampohlenz.com>
2012-03-03 11:03:11 +1030

    Use asset_path helpers to determine TinyMCE base path when not explicitly set

--- lib/tinymce/rails.rb
+++ lib/tinymce/rails.rb
@@ -2,12 +2,5 @@ module TinyMCE
   module Rails
     require 'tinymce/rails/engine'
     require 'tinymce/rails/version'
-
-    def self.base
-      Engine.config.tinymce.base ||
-        [::Rails.application.config.action_controller.asset_host,
-         ActionController::Base.config.relative_url_root,
-         ::Rails.application.config.assets.prefix, '/tinymce'].compact.join
-    end
   end
 end
--- lib/tinymce/rails/engine.rb
+++ lib/tinymce/rails/engine.rb
@@ -2,6 +2,9 @@ module TinyMCE
   module Rails
     class Engine < ::Rails::Engine
       config.tinymce = ActiveSupport::OrderedOptions.new
+      
+      # Set an explicit base path for TinyMCE assets (usually defaults to /assets/tinymce)
+      config.tinymce.base = nil
     end
   end
 end
