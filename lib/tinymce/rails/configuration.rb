ad8b625d96cfc66cdfc9c855115ff4c20ca87d64
Sam Pohlenz <sam@sampohlenz.com>
2012-05-26 10:00:16 +0200

    Explicitly cast configuration values to string before checking if JS function (#62)

--- lib/tinymce/rails/configuration.rb
+++ lib/tinymce/rails/configuration.rb
@@ -26,7 +26,7 @@ module TinyMCE::Rails
       options.each do |key, value|
         if value.is_a?(Array) && value.all? { |v| v.is_a?(String) }
           result[key] = value.join(",")
-        elsif value.starts_with?("function(")
+        elsif value.to_s.starts_with?("function(")
           result[key] = Function.new(value)
         else
           result[key] = value
