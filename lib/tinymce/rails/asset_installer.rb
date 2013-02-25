c720d26bf17c530ce473dbcff55bf742b84f8a39
Sam Pohlenz <sam@sampohlenz.com>
2013-02-25 16:20:09 +1030

    Add manifest cleanup and specs for AssetInstaller

--- lib/tinymce/rails/asset_installer.rb
+++ lib/tinymce/rails/asset_installer.rb
@@ -11,14 +11,26 @@ module TinyMCE
       end
       
       def install
+        cleanup_assets
         copy_assets
         append_to_manifest
+        
         manifest.write
       end
     
     private
       def manifest
         @manifest ||= AssetManifest.load(@manifest_path)
+      end
+      
+      def cleanup_assets
+        manifest.each(/^tinymce\//) do |asset|
+          manifest.remove(asset) if index_asset?(asset)
+          
+          manifest.remove_digest(asset) do |src, dest|
+            move_asset(src, dest)
+          end
+        end
       end
       
       def copy_assets
@@ -34,6 +46,17 @@ module TinyMCE
       
       def asset_files
         Pathname.glob("#{ASSETS}/**/*").select(&:file?)
+      end
+      
+      def move_asset(src, dest)
+        src = File.join(@target, src)
+        dest = File.join(@target, dest)
+        
+        FileUtils.mv(src, dest) if File.exists?(src)
+      end
+      
+      def index_asset?(asset)
+        asset =~ /\/index\.js$/
       end
     end
   end
---
+++ spec/lib/asset_installer_spec.rb
@@ -0,0 +1,59 @@
+require "tinymce/rails/asset_installer"
+
+module TinyMCE
+  module Rails
+    describe AssetInstaller do
+      before(:each) do
+        FileUtils.stub(:cp_r)
+        FileUtils.stub(:mv)
+        
+        stub_const("TinyMCE::Rails::AssetManifest", stub(:load => manifest))
+      end
+      
+      let(:target) { "/assets" }
+      let(:manifest_path) { nil }
+      let(:manifest) { stub.as_null_object }
+      
+      def install
+        AssetInstaller.new(target, manifest_path).install
+      end
+      
+      it "removes TinyMCE index assets" do
+        asset = "tinymce/langs/es/index.js"
+        
+        manifest.stub(:each).and_yield(asset)
+        manifest.should_receive(:remove).with(asset)
+        
+        install
+      end
+      
+      it "removes digests from existing TinyMCE assets in the manifest" do
+        digested_asset = "tinymce/langs/es-abcde1234567890.js"
+        asset = "tinymce/langs/es.js"
+        
+        manifest.stub(:each).and_yield(asset)
+        manifest.should_receive(:remove_digest).with(asset).and_yield(digested_asset, asset)
+        File.stub(:exists?).and_return(true)
+        FileUtils.should_receive(:mv).with("/assets/tinymce/langs/es-abcde1234567890.js", "/assets/tinymce/langs/es.js")
+        
+        install
+      end
+      
+      it "copies TinyMCE assets to the target directory" do
+        FileUtils.should_receive(:cp_r).with(AssetInstaller::ASSETS, target, :preserve => true)
+        install
+      end
+      
+      it "adds TinyMCE assets to the manifest" do
+        manifest.should_receive(:append).with("tinymce/tiny_mce.js")
+        manifest.should_receive(:append).with("tinymce/themes/advanced/editor_template.js")
+        install
+      end
+      
+      it "writes the manifest" do
+        manifest.should_receive(:write)
+        install
+      end
+    end
+  end
+end
