951dbeb8d6ec217718e43e76e8fedd3c8cfc51f0
Sam Pohlenz <sam@sampohlenz.com>
2013-05-05 16:32:26 +0930

    Update specs and sandbox app config for Rails 4

--- sandbox/config/initializers/secret_token.rb
+++ sandbox/config/initializers/secret_token.rb
@@ -5,3 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Sandbox::Application.config.secret_token = '10b31f60e3e66ae0d844e2dda97aa51d731e1623401e82bd2b82e5a7c62b974c582d01238d7f928125326122853c3e8d973b26610b9aaee9f29416359c7a26cc'
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandbox::Application.config.secret_key_base = '10b31f60e3e66ae0d844e2dda97aa51d731e1623401e82bd2b82e5a7c62b974c582d01238d7f928125326122853c3e8d973b26610b9aaee9f29416359c7a26cc'
