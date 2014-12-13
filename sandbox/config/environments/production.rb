7d873f820527ac45743beeabe44148349e4758b2
Sam Pohlenz <sam@sampohlenz.com>
2014-12-14 10:07:02 +1030

    Defined JS compressor for sandbox

--- sandbox/config/environments/production.rb
+++ sandbox/config/environments/production.rb
@@ -17,8 +17,9 @@ Sandbox::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
