6213b43c616a211997720f1b0cb4b82a229c9d1f
Sam Pohlenz <sam@sampohlenz.com>
2014-12-14 16:17:11 +1030

    DRY up asset installers

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.18'
+gem 'rails', '~> 4.1.8'
+#gem 'rails', '~> 4.0.12'
+#gem 'rails', '~> 3.2.18'
 
 gem 'jquery-rails'
 gem 'uglifier'
--- lib/tinymce/rails/asset_manifest.rb
+++ lib/tinymce/rails/asset_manifest.rb
@@ -8,126 +8,133 @@ module TinyMCE
           JsonManifest.try(manifest_path) ||
           NullManifest.new
       end
-    end
-  end
-  
-  class YamlManifest
-    def self.try(manifest_path)
-      yaml_file = File.join(manifest_path, "manifest.yml")
-      new(yaml_file) if File.exists?(yaml_file)
-    end
-    
-    def initialize(file)
-      @file = file
-      @manifest = YAML.load_file(file)
-    end
-    
-    def append(logical_path, file)
-      @manifest[logical_path] = logical_path
-    end
-    
-    def remove(logical_path)
-      @manifest.delete(logical_path)
-    end
-    
-    def remove_digest(logical_path)
-      asset_path(logical_path) do |digested, logical_path|
-        yield digested, logical_path if block_given?
-      end
-    end
-    
-    def asset_path(logical_path)
-      if digested = @manifest[logical_path]
-        yield digested, logical_path if block_given?
-      end
-    end
-    
-    def each(pattern)
-      @manifest.each_key do |asset|
-        yield asset if asset =~ pattern
-      end
-    end
-    
-    def to_s
-      dump
-    end
-    
-    def dump(io=nil)
-      YAML.dump(@manifest, io)
-    end
-    
-    def write
-      File.open(@file, "wb") { |f| dump(f) }
-    end
-  end
-  
-  class JsonManifest
-    def self.try(manifest_path)
-      paths = Dir[File.join(manifest_path, "manifest*.json")]
-      new(paths.first) if paths.any?
-    end
-    
-    def initialize(file)
-      @file = file
-      @manifest = MultiJson.load(File.read(file))
-    end
-    
-    def append(logical_path, file)
-      stat = File.stat(file)
       
-      @manifest["assets"][logical_path] = logical_path
-      @manifest["files"][logical_path] = {
-        "logical_path" => logical_path,
-        "mtime"        => stat.mtime.iso8601,
-        "size"         => stat.size,
-        "digest"       => nil
-      }
-    end
+      def each(pattern)
+        assets.each_key do |asset|
+          if asset =~ pattern && !index_asset?(asset)
+            yield asset
+          end
+        end
+      end
+      
+      def asset_path(logical_path)
+        if digested = assets[logical_path]
+          yield digested, logical_path if block_given?
+        end
+      end
+      
+      def to_s
+        dump
+      end
     
-    def remove(logical_path)
-      if digested = @manifest["assets"].delete(logical_path)
-        @manifest["files"].delete(digested)
+    protected
+      def index_asset?(asset)
+        asset =~ /\/index[^\/]*\.\w+$/
       end
     end
-    
-    def remove_digest(logical_path)
-      asset_path(logical_path) do |digested, logical_path|
-        @manifest["assets"][logical_path] = logical_path
-        @manifest["files"][logical_path] = @manifest["files"].delete(digested).tap { |f| f["digest"] = nil }
-        yield digested, logical_path if block_given?
-      end
-    end
-    
-    def asset_path(logical_path)
-      if digested = @manifest["assets"][logical_path]
-        yield digested, logical_path if block_given?
-      end
-    end
-    
-    def each(pattern)
-      @manifest["assets"].each_key do |asset|
-        yield asset if asset =~ pattern
-      end
-    end
-    
-    def to_s
-      dump
-    end
-    
-    def dump
-      MultiJson.dump(@manifest)
-    end
-    
-    def write
-      File.open(@file, "wb") { |f| f.write(dump) }
-    end
-  end
   
-  class NullManifest
-    def append(*); end
-    def remove(*); end
-    def remove_digest(*); end
-    def each(*); end
-    def write; end
+    class YamlManifest < AssetManifest
+      def self.try(manifest_path)
+        yaml_file = File.join(manifest_path, "manifest.yml")
+        new(yaml_file) if File.exists?(yaml_file)
+      end
+    
+      def initialize(file)
+        @file = file
+        @manifest = YAML.load_file(file)
+      end
+    
+      def append(logical_path, file)
+        assets[logical_path] = logical_path
+      end
+    
+      def remove(logical_path)
+        assets.delete(logical_path)
+      end
+    
+      def remove_digest(logical_path)
+        asset_path(logical_path) do |digested, logical_path|
+          assets[logical_path] = logical_path
+          
+          yield digested, logical_path if block_given?
+        end
+      end
+    
+      def assets
+        @manifest
+      end
+    
+      def dump(io=nil)
+        YAML.dump(@manifest, io)
+      end
+    
+      def write
+        File.open(@file, "wb") { |f| dump(f) }
+      end
+    end
+  
+    class JsonManifest < AssetManifest
+      def self.try(manifest_path)
+        paths = Dir[File.join(manifest_path, "manifest*.json")]
+        new(paths.first) if paths.any?
+      end
+    
+      def initialize(file)
+        @file = file
+        @manifest = MultiJson.load(File.read(file))
+      end
+    
+      def append(logical_path, file)
+        stat = File.stat(file)
+      
+        assets[logical_path] = logical_path
+        files[logical_path] = {
+          "logical_path" => logical_path,
+          "mtime"        => stat.mtime.iso8601,
+          "size"         => stat.size,
+          "digest"       => nil
+        }
+      end
+    
+      def remove(logical_path)
+        if digested = assets.delete(logical_path)
+          files.delete(digested)
+        end
+      end
+    
+      def remove_digest(logical_path)
+        asset_path(logical_path) do |digested, logical_path|
+          assets[logical_path] = logical_path
+          files[logical_path] = files.delete(digested).tap { |f| f["digest"] = nil }
+          
+          yield digested, logical_path if block_given?
+        end
+      end
+      
+      def assets
+        @manifest["assets"]
+      end
+      
+      def files
+        @manifest["files"]
+      end
+    
+      def dump
+        MultiJson.dump(@manifest)
+      end
+    
+      def write
+        File.open(@file, "wb") { |f| f.write(dump) }
+      end
+    end
+  
+    class NullManifest < AssetManifest
+      def append(*); end
+      def remove(*); end
+      def remove_digest(*); end
+      def assets; {}; end
+      def each(*); end
+      def write; end
+    end
   end
 end
