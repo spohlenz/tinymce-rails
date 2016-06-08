068caeccb0c1d6d1f358dcf7ce8a781ed1bd0273
Sebastian Prestel <sebastian.prestel@infopark.de>
2016-06-08 15:33:37 +0200

    Remove automatic language detection as it conflicts with sprockets-rails 3.x
    * copy and own of https://github.com/spohlenz/tinymce-rails/commit/dbb107cdf40a22b9c1f25782ec501e6f2b519341

--- lib/tinymce/rails/configuration.rb
+++ lib/tinymce/rails/configuration.rb
@@ -7,7 +7,7 @@ module TinyMCE::Rails
         self
       end
     end
-    
+
     def self.defaults
       {
         "mode"            => "specific_textareas",
@@ -15,22 +15,22 @@ module TinyMCE::Rails
         "theme"           => "advanced"
       }
     end
-    
+
     attr_reader :options
-    
+
     def initialize(options)
       @options = options
     end
-    
+
     def self.new_with_defaults(options={})
       config = new(defaults)
       config = config.merge(options) if options
       config
     end
-    
+
     def options_for_tinymce
       result = {}
-      
+
       options.each do |key, value|
         if value.is_a?(Array) && value.all? { |v| v.is_a?(String) }
           result[key] = value.join(",")
@@ -40,12 +40,10 @@ module TinyMCE::Rails
           result[key] = value
         end
       end
-      
-      result["language"] ||= self.class.default_language
-      
+
       result
     end
-    
+
     def to_javascript
       pairs = options_for_tinymce.inject([]) do |result, (k, v)|
         if v.respond_to?(:to_javascript)
@@ -53,54 +51,25 @@ module TinyMCE::Rails
         elsif v.respond_to?(:to_json)
           v = v.to_json
         end
-        
+
         result << [k, v].join(": ")
       end
-      
+
       "{\n#{pairs.join(",\n")}\n}"
     end
-    
+
     def merge(options)
       self.class.new(self.options.merge(options))
     end
-    
-    # Default language falls back to English if current locale is not available.
-    def self.default_language
-      available_languages.include?(I18n.locale.to_s) ? I18n.locale.to_s : "en"
-    end
-    
-    # Searches asset paths for TinyMCE language files.
-    def self.available_languages
-      assets.paths.map { |path|
-        # Find all assets within tinymce/langs
-        entries = assets.entries(File.join(path, "tinymce/langs"))
-        entries.map { |entry|
-          if assets.respond_to?(:attributes_for)
-            assets.attributes_for(File.join(path, entry))
-          else
-            assets.find_asset(File.join("tinymce/langs", entry))
-          end
-        }.select { |asset|
-          # Select only JavaScript files
-          asset.logical_path =~ /\.js$/
-        }.map { |asset|
-          # Strip path and extension
-          asset.logical_path.sub(/^tinymce\/langs\//, "").sub(/\.js$/, "")
-        }
-      }.flatten.uniq
-    end
-  
-    def self.assets
-      Rails.application.assets
-    end
+
   end
-  
+
   class MultipleConfiguration < ActiveSupport::HashWithIndifferentAccess
     def initialize(configurations={})
       configurations = configurations.each_with_object({}) { |(name, options), h|
         h[name] = Configuration.new_with_defaults(options)
       }
-      
+
       super(configurations)
     end
   end
