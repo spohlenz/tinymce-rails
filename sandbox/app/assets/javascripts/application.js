789c46759423fc5edfa940cacf0506681d917397
Sam Pohlenz <sam@sampohlenz.com>
2019-02-22 09:35:18 +1030

    Update Sandbox to work with CSP and Turbolinks

--- sandbox/app/assets/javascripts/application.js
+++ sandbox/app/assets/javascripts/application.js
@@ -1,1 +1,7 @@
 //= require turbolinks
+
+document.addEventListener("turbolinks:request-start", function(event) {
+  var xhr = event.data.xhr;
+  var nonce = document.querySelectorAll("meta[name='csp-nonce']")[0].content;
+  xhr.setRequestHeader("X-Turbolinks-Nonce", nonce);
+});
