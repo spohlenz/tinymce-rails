d9bd91894803c6cab639db1ddaf8e62d105114cb
Sam Pohlenz <sam@sampohlenz.com>
2019-02-22 09:41:08 +1030

    Check for content security policy support in sandbox

--- sandbox/config/initializers/content_security_policy.rb
+++ sandbox/config/initializers/content_security_policy.rb
@@ -1,32 +1,33 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+if Rails.application.config.respond_to?(:content_security_policy)
+  # Define an application-wide content security policy
+  # For further information see the following documentation
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+  Rails.application.config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https
+    policy.style_src   :self, :https
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
-  policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
-
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
-end
-
-# If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = -> (request) do
-  # use the same csp nonce for turbolinks requests
-  if request.env['HTTP_TURBOLINKS_REFERRER'].present?
-    request.env['HTTP_X_TURBOLINKS_NONCE']
-  else
-    SecureRandom.base64(16)
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
   end
-end
 
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+  # If you are using UJS then enable automatic nonce generation
+  Rails.application.config.content_security_policy_nonce_generator = -> (request) do
+    # use the same csp nonce for turbolinks requests
+    if request.env['HTTP_TURBOLINKS_REFERRER'].present?
+      request.env['HTTP_X_TURBOLINKS_NONCE']
+    else
+      SecureRandom.base64(16)
+    end
+  end
+
+  # Report CSP violations to a specified URI
+  # For further information see the following documentation:
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+  # Rails.application.config.content_security_policy_report_only = true
+end
