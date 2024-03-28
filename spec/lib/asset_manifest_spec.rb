adeafe69a0a2d5e5e33ef9ee9486bb5f4a70635b
Sam Pohlenz <sam@sampohlenz.com>
2024-03-28 13:29:01 +1030

    Fix asset manifest loading when a specific JSON manifest path is provided

--- spec/lib/asset_manifest_spec.rb
+++ spec/lib/asset_manifest_spec.rb
@@ -8,19 +8,25 @@ module TinyMCE
       end
 
       describe ".load" do
-        it "returns a YamlManifest if a YAML manifest file exists" do
+        it "returns a YamlManifest if a YAML manifest file exists within the given path" do
           manifest = AssetManifest.load(fixture("yaml_manifest"))
           expect(manifest).to be_an_instance_of(YamlManifest)
           expect(manifest.file).to eq fixture("yaml_manifest/manifest.yml")
         end
 
-        it "returns a JsonManifest if a JSON manifest file exists" do
+        it "returns a JsonManifest if a JSON manifest file exists within the given path" do
           manifest = AssetManifest.load(fixture("json_manifest"))
           expect(manifest).to be_an_instance_of(JsonManifest)
           expect(manifest.file).to eq fixture("json_manifest/.sprockets-manifest-18802ea98f713a419dac90694dd5b6c4.json")
         end
 
-        it "returns a JsonManifest if a legacy JSON manifest file exists" do
+        it "returns a JsonManifest if a specific JSON manifest file is provided" do
+          manifest = AssetManifest.load(fixture("json_manifest.json"))
+          expect(manifest).to be_an_instance_of(JsonManifest)
+          expect(manifest.file).to eq fixture("json_manifest.json")
+        end
+
+        it "returns a JsonManifest if a legacy JSON manifest file exists within the given path" do
           manifest = AssetManifest.load(fixture("legacy_manifest"))
           expect(manifest).to be_an_instance_of(JsonManifest)
           expect(manifest.file).to eq fixture("legacy_manifest/manifest-18802ea98f713a419dac90694dd5b6c4.json")
