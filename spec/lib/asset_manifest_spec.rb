f46dda0dcd3aff44205110331cde194aeb60ed80
Sam Pohlenz <sam@sampohlenz.com>
2024-07-10 15:44:58 +0930

    Implement PropshaftManifest

--- spec/lib/asset_manifest_spec.rb
+++ spec/lib/asset_manifest_spec.rb
@@ -8,6 +8,12 @@ module TinyMCE
       end
 
       describe ".load" do
+        it "returns a PropshaftManifest if a Propshaft manifest file exists within the given path" do
+          manifest = AssetManifest.load(fixture("propshaft_manifest"))
+          expect(manifest).to be_an_instance_of(PropshaftManifest)
+          expect(manifest.file).to eq fixture("propshaft_manifest/.manifest.json")
+        end
+
         it "returns a YamlManifest if a YAML manifest file exists within the given path" do
           manifest = AssetManifest.load(fixture("yaml_manifest"))
           expect(manifest).to be_an_instance_of(YamlManifest)
@@ -103,7 +109,7 @@ module TinyMCE
         end
       end
 
-      describe "JSON manifest" do
+      describe JsonManifest do
         subject(:manifest) { JsonManifest.new(fixture("json_manifest/.sprockets-manifest-18802ea98f713a419dac90694dd5b6c4.json")) }
 
         def reload_manifest(manifest)
@@ -189,6 +195,75 @@ module TinyMCE
 
           it "returns nil if no JSON manifest was found" do
             expect(JsonManifest.try(fixture("no_manifest"), ".sprockets-manifest*.json")).to be_nil
+          end
+        end
+      end
+
+      describe PropshaftManifest do
+        subject(:manifest) { PropshaftManifest.new(fixture("propshaft_manifest/.manifest.json")) }
+
+        def reload_manifest(manifest)
+          JSON.parse(manifest.to_s)
+        end
+
+        it "keeps existing manifest data" do
+          result = reload_manifest(manifest)
+          expect(result["application.css"]).to eq("application-c2cd8c57.css")
+          expect(result["application.js"]).to eq("application-bfcdf840.js")
+          expect(result["tinymce/tinymce.js"]).to eq("tinymce/tinymce-52aa7906.js")
+        end
+
+        describe "#append" do
+        let(:file) { double }
+
+          it "adds files to the manifest without a fingerprint" do
+            manifest.append("tinymce/tinymce.js", file)
+
+            result = reload_manifest(manifest)
+            expect(result["tinymce/tinymce.js"]).to eq("tinymce/tinymce.js")
+          end
+        end
+
+        describe "#remove" do
+          it "removes files from the manifest" do
+            manifest.remove("tinymce/tinymce.js")
+
+            result = reload_manifest(manifest)
+            expect(result).to_not have_key("tinymce/tinymce.js")
+          end
+        end
+
+        describe "#remove_digest" do
+          it "sets the file digest value to its non-digested version" do
+            manifest.remove_digest("tinymce/tinymce.js")
+
+            result = reload_manifest(manifest)
+            expect(result["tinymce/tinymce.js"]).to eq("tinymce/tinymce.js")
+          end
+
+          it "yields the digested and non-digested file names" do
+            expect { |block|
+              manifest.remove_digest("tinymce/tinymce.js", &block)
+            }.to yield_with_args("tinymce/tinymce-52aa7906.js", "tinymce/tinymce.js")
+          end
+        end
+
+        describe "#each" do
+          it "yields the logical path for each asset that matches the given pattern" do
+            result = []
+            manifest.each(/^tinymce/) { |asset| result << asset }
+            expect(result).to eq(["tinymce/tinymce.js"])
+          end
+        end
+
+        describe ".try" do
+          it "returns a new JsonManifest if a JSON manifest exists for the given path" do
+            manifest = PropshaftManifest.try(fixture("propshaft_manifest"))
+            expect(manifest).to be_an_instance_of(PropshaftManifest)
+          end
+
+          it "returns nil if no JSON manifest was found" do
+            expect(PropshaftManifest.try(fixture("no_manifest"))).to be_nil
           end
         end
       end
