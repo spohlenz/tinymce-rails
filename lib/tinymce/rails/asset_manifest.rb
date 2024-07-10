94d6d0a23f25ae354b5bee9880c50009142f48fe
Sam Pohlenz <sam@sampohlenz.com>
2024-07-10 15:44:58 +0930

    Implement PropshaftManifest

--- lib/tinymce/rails/asset_manifest.rb
+++ lib/tinymce/rails/asset_manifest.rb
@@ -4,7 +4,8 @@ module TinyMCE
       attr_reader :file
 
       def self.load(manifest_path)
-        JsonManifest.try(manifest_path, ".sprockets-manifest*.json") ||
+        PropshaftManifest.try(manifest_path) ||
+          JsonManifest.try(manifest_path, ".sprockets-manifest*.json") ||
           JsonManifest.try(manifest_path, "manifest*.json") ||
           JsonManifest.try(manifest_path) ||
           YamlManifest.try(manifest_path) ||
@@ -123,6 +124,46 @@ module TinyMCE
 
       def files
         @manifest["files"]
+      end
+
+      def dump
+        JSON.generate(@manifest)
+      end
+
+      def write
+        File.open(@file, "wb") { |f| f.write(dump) }
+      end
+    end
+
+    class PropshaftManifest < AssetManifest
+      def self.try(manifest_path)
+        json_file = File.join(manifest_path, ".manifest.json")
+        new(json_file) if File.exist?(json_file)
+      end
+
+      def initialize(file)
+        @file = file
+        @manifest = JSON.parse(File.read(file))
+      end
+
+      def append(logical_path, file)
+        assets[logical_path] = logical_path
+      end
+
+      def remove(logical_path)
+        assets.delete(logical_path)
+      end
+
+      def remove_digest(logical_path)
+        asset_path(logical_path) do |digested, logical_path|
+          assets[logical_path] = logical_path
+
+          yield digested, logical_path if block_given?
+        end
+      end
+
+      def assets
+        @manifest
       end
 
       def dump
